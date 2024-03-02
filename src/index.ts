f4afa9137720fd4fb9726323365f4c934079cdb6
antn9x <antn9x@gmail.com>
2024-03-02 21:08:24 +0700

    fix export color

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,34 @@ export { initWorld } from './app'
 export { GameWorld } from './gworld'
 export { BoxCollider, Collider } from './gworld/components/CollideComponent'
 export {
-  BlockInputEventsComp, ButtonComp, FillType, LabelComp,
-  LabelOutlineComp, LabelShadowComp, ProgressTimerComp, RichTextComp, ScrollViewComp
+  BlockInputEventsComp,
+  ButtonComp,
+  FillType,
+  LabelComp,
+  LabelOutlineComp,
+  LabelShadowComp,
+  ProgressTimerComp,
+  RichTextComp,
+  ScrollViewComp,
 } from './gworld/components/GUIComponent'
-export { EventRegister, ExtraDataComp, TouchEventRegister } from './gworld/components/NoRenderComponent'
 export { NodeComp } from './gworld/components/NodeComp'
-export { GraphicsRender, NodeRender, ParticleComp, SpineSkeleton, SpriteRender } from './gworld/components/RenderComponent'
-export { SceneComponent } from './gworld/core/Scene'
+export {
+  EventRegister,
+  ExtraDataComp,
+  TouchEventRegister,
+} from './gworld/components/NoRenderComponent'
+export {
+  GraphicsRender,
+  NodeRender,
+  ParticleComp,
+  SpineSkeleton,
+  SpriteRender,
+} from './gworld/components/RenderComponent'
 export { ComponentX, NoRenderComponentX } from './gworld/core/decorator'
+export { SceneComponent } from './gworld/core/Scene'
 export { CollideSystem } from './gworld/systems/CollideSystem'
-export { instantiate, registerSystemFnc as registerSystem } from './helper/utils'
-export { Size, Vec2, type Touch } from './polyfills'
+export {
+  instantiate,
+  registerSystemFnc as registerSystem,
+} from './helper/utils'
+export { Color4B, Size, type Touch, Vec2 } from './polyfills'
