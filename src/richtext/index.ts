b0a56e22198daf75e27eb10c3b9c10bbed649b33
antn1 <antn@ftech.com.vn>
2025-07-24 11:39:19 +0700

    fix richtext

--- src/richtext/index.ts
+++ src/richtext/index.ts
@@ -1,6 +1,8 @@
 import { GameWorld } from '../gworld'
 import { RichTextSystem } from './RichTextSystem'
 
+export * from './RichTextComp'
+
 export function setupRichText() {
   GameWorld.Instance.systems.add(RichTextSystem)
   GameWorld.Instance.systems.configureOnce(RichTextSystem)
